e095d6b39930f4209d0a872b5e0d3f7f07ebd93d
Frankie <1426203851@qq.com>
2020-09-10 23:51:37 +0800

    husky test

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,7 @@ Page({
     })
   },
   onLoad: function () {
+    console.log('pre-commit test')
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
