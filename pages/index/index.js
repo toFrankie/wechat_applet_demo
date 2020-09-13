8bd96754174910cd595cf148561495c1944eb68c
Frankie <1426203851@qq.com>
2020-09-13 17:17:41 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,8 +16,6 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('pre-commit test')
-    console.log('pre-commit test')
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
